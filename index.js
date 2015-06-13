081f39d4e1e9616f1491e3626715648a95bb4d06
Markus Tarkiainen <markus@skydom.io>
2015-06-12 22:25:31 -0700

    Added container paths

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ module.exports = function(jflo) {
             return eventstream.through(function(data) {
                 var group_id = jflo.util.fget(data, groupby_field_path) || "@uncategorized";
                 var group = stats_by_group[group_id] = stats_by_group[group_id] || {};
-                var flat_data = dutil.flatten('', data, {delimiter: DELIMITER});
+                var flat_data = dutil.flatten('', data, {
+                    delimiter: DELIMITER,
+                    container_paths: true
+                });
                 group['@document_count'] = (group['@document_count'] || 0) + 1;
                 Object.keys(flat_data).forEach(function(path) {
                     var abstract_path = path.split(DELIMITER)
